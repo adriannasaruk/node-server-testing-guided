3bf84fe6986a3cf5d5460d9e17efd1d03a4a151e
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-04-23 06:46:17 -1000

    updated

--- knexfile.js
+++ knexfile.js
@@ -25,15 +25,16 @@ module.exports = {
       directory: "./data/seeds",
     },
     //Heroku will look for a production config
-    production: {
-      client: "pg", // npm i pg
-      connection: process.env.DATABASE_URL, // provided by heroku
-      migrations: {
-        directory: "./data/migrations",
-      },
-      seeds: {
-        directory: "./data/seeds",
-      },
+   
+  },
+  production: {
+    client: "pg", // npm i pg
+    connection: process.env.DATABASE_URL, // provided by heroku
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
     },
   },
 };
